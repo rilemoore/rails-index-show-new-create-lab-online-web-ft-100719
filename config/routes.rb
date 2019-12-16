46bdd09a3c39537bb25acceb0cd532c2f8b6c557
Riley Moore <rilemoore@gmail.com>
2019-12-16 00:58:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :coupons, only: [:index, :new, :create]
+  resources :coupons
   get '/coupons/:id', to: 'coupons#show', as: 'coupon'
 end
