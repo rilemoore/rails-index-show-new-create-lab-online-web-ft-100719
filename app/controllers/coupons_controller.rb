168bcaf413b767fe1671f4a24405d898e2e98b6a
Riley Moore <rilemoore@gmail.com>
2019-12-16 00:50:29 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,8 +9,12 @@ class CouponsController < ApplicationController
   end
   
   def create
-    # Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    # redirect_to students_path
+    @coupon = Coupon.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+
+    redirect_to student_path(@student)
   end
 
 end
